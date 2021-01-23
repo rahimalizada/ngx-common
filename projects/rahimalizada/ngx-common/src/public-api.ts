33099869f675dad1e628651aa4d23bd5583bcd45
Rahim Alizada <rahim.alizada@gmail.com>
2021-01-23 18:21:44 +0400

    Added AuthData model class

--- projects/rahimalizada/ngx-common/src/public-api.ts
+++ projects/rahimalizada/ngx-common/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngx-common
  */
 
+export * from './lib/model/auth/auth-data.model';
 export * from './lib/model/mongodb/object-id.model';
 export * from './lib/model/with-id.model';
 export * from './lib/ngx-common.module';
