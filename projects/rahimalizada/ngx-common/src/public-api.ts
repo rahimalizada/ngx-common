e1ccca8ee491e93f319bb490fe0eaea40c9a63a5
Rahim Alizada <rahim.alizada@gmail.com>
2021-01-23 18:20:13 +0400

    Added ObjectId model class

--- projects/rahimalizada/ngx-common/src/public-api.ts
+++ projects/rahimalizada/ngx-common/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ngx-common
  */
 
+export * from './lib/model/mongodb/object-id.model';
 export * from './lib/model/with-id.model';
 export * from './lib/ngx-common.module';
