13c8353065496f834e2062d5f9caa66314da585b
Rahim Alizada <rahim.alizada@gmail.com>
2021-01-23 22:24:21 +0400

    Merged @rahimalizada/ngx-pipes library

--- projects/rahimalizada/ngx-common/src/lib/ngx-common.module.ts
+++ projects/rahimalizada/ngx-common/src/lib/ngx-common.module.ts
@@ -1,11 +1,14 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { SafePipe } from './pipes/safe.pipe';
+import { ValueOrDashPipe } from './pipes/value-or-dash.pipe';
+import { YesNoPipe } from './pipes/yes-no.pipe';
 import { DateParserInterceptor } from './rest/date-parser.interceptor';
 
 @NgModule({
-  declarations: [],
+  declarations: [YesNoPipe, ValueOrDashPipe, SafePipe],
   imports: [HttpClientModule],
-  exports: [],
+  exports: [YesNoPipe, ValueOrDashPipe, SafePipe],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
