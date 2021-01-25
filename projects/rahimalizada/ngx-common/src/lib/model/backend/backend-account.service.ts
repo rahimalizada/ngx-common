ff609093579c6e6d52b83720e6fc5a561e072087
Rahim Alizada <rahim.alizada@gmail.com>
2021-01-25 21:26:50 +0400

    Modified BackendAccountService resource path

--- projects/rahimalizada/ngx-common/src/lib/model/backend/backend-account.service.ts
+++ projects/rahimalizada/ngx-common/src/lib/model/backend/backend-account.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { AbstractRestService } from '../../rest/abstract-rest.service';
 import { AuthResult } from '../auth/auth-result.model';
 import { BackendAccount } from './backend-account.model';
@@ -9,10 +10,10 @@ import { BackendAccount } from './backend-account.model';
 })
 export class BackendAccountService extends AbstractRestService<BackendAccount> {
   constructor(httpClient: HttpClient) {
-    super(httpClient, '/api/account');
+    super(httpClient, '/api/backend-account');
   }
 
-  changePassword(value: { currentPassword: string; newPassword: string }) {
+  changePassword(value: { currentPassword: string; newPassword: string }): Observable<AuthResult<BackendAccount, string>> {
     return this.httpClient.post<AuthResult<BackendAccount, string>>(`${this.basePath}/password`, value);
   }
 }
