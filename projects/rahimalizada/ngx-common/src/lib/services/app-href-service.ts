352fce01dfd38d877f4160aac88badc13d7e2928
Rahim Alizada <rahim.alizada@gmail.com>
2021-01-31 14:33:56 +0400

    Fixed a bug in AppHrefService

--- projects/rahimalizada/ngx-common/src/lib/services/app-href-service.ts
+++ projects/rahimalizada/ngx-common/src/lib/services/app-href-service.ts
@@ -12,7 +12,7 @@ export class AppHrefService {
   constructor(locationStrategy: LocationStrategy, @Inject(DOCUMENT) document: Document) {
     this.appOrigin = document.location.origin;
 
-    const base = locationStrategy.getBaseHref() === document.location.origin ? '/' : document.location.origin;
+    const base = locationStrategy.getBaseHref() === document.location.origin ? '/' : locationStrategy.getBaseHref();
     this.appBaseHref = new URL(base, document.location.origin).href;
   }
 }
