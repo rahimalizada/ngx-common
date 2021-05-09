b6263dd22f8b632d775e7124fa20dfea144a0452
Rahim Alizada <rahim.alizada@gmail.com>
2021-05-09 10:10:15 +0400

    Updated page reload helper

--- projects/rahimalizada/ngx-common/src/lib/helpers/reload-page.helper.ts
+++ projects/rahimalizada/ngx-common/src/lib/helpers/reload-page.helper.ts
@@ -1,6 +1,6 @@
 import { ActivatedRoute, Router } from '@angular/router';
 
-export const reloadPage = (router: Router, activatedRoute: ActivatedRoute): void => {
+export function reloadPage(router: Router, activatedRoute: ActivatedRoute): void {
   router.navigated = false;
   void router.navigate(['./'], { relativeTo: activatedRoute, queryParamsHandling: 'preserve' });
-};
+}
