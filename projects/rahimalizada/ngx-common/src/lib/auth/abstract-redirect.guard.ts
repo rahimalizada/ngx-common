fb24f9da810c828424cbc0a53586498dcdb6de0a
Rahim Alizada <rahim.alizada@gmail.com>
2021-05-18 20:10:50 +0400

    Modified AbstractRedirectGuard

--- projects/rahimalizada/ngx-common/src/lib/auth/abstract-redirect.guard.ts
+++ projects/rahimalizada/ngx-common/src/lib/auth/abstract-redirect.guard.ts
@@ -1,12 +1,8 @@
-import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class RedirectGuard implements CanActivate, CanActivateChild {
-  constructor(protected authService: { isLoggedIn: () => boolean }, protected router: Router) {}
+export abstract class AbstractRedirectGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: { isLoggedIn: () => boolean }, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | boolean | UrlTree {
     const loggedInPath = route.data.loggedInPath ?? '/';
