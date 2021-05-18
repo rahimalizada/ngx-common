8b69f306cdce61e45044a5e14b90c75a8555f167
Rahim Alizada <rahim.alizada@gmail.com>
2021-05-18 20:03:37 +0400

    Modified RedirectGuard

--- projects/rahimalizada/ngx-common/src/lib/auth/redirect.guard.ts
+++ projects/rahimalizada/ngx-common/src/lib/auth/redirect.guard.ts
@@ -1,6 +1,10 @@
+import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class RedirectGuard implements CanActivate, CanActivateChild {
   constructor(protected authService: { isLoggedIn: () => boolean }, protected router: Router) {}
 
