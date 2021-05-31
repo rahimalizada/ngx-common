ff3344a688d9dd830499a0a1df6a8e60029d7ff2
Rahim Alizada <rahim.alizada@gmail.com>
2021-05-31 19:11:47 +0400

    Updated build script

--- .patch-version-and-publish.cmd
+++ .patch-version-and-publish.cmd
@@ -1,4 +1,5 @@
 set name=ngx-common
+set distDir=%~dp0dist
 
 call npm version patch --prefix .\projects\rahimalizada\%name%\
 git add .\projects\rahimalizada\%name%\package.json
@@ -10,5 +11,8 @@ git push origin --tags
 
 call ng build @rahimalizada/%name% --configuration production
 call npm publish .\dist\rahimalizada\%name%\ --access public
+
+call "D:\user\Documents\dev\_other\shell\scripts\node_clear_dist.cmd" %distDir%
+
 
 pause
