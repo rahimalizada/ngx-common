1f130a4c96d6aeb0d1be8e38bcc6c67c586c7438
Rahim Alizada <rahim.alizada@gmail.com>
2021-06-01 09:39:47 +0400

    Updated reloadPage helper

--- projects/rahimalizada/ngx-common/src/lib/helpers/reload-page.helper.ts
+++ projects/rahimalizada/ngx-common/src/lib/helpers/reload-page.helper.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 export function reloadPage(router: Router, activatedRoute: ActivatedRoute): void {
+  router.routeReuseStrategy.shouldReuseRoute = () => false;
   router.navigated = false;
   void router.navigate(['./'], { relativeTo: activatedRoute, queryParamsHandling: 'preserve' });
 }
