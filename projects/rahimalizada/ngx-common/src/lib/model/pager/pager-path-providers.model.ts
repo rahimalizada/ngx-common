e565a50fa90983fd713ad96e72d2a51625b1363d
Rahim Alizada <rahim.alizada@gmail.com>
2021-06-03 17:39:57 +0400

    Added PagerPathProviders

--- projects/rahimalizada/ngx-common/src/lib/model/pager/pager-path-providers.model.ts
+++ projects/rahimalizada/ngx-common/src/lib/model/pager/pager-path-providers.model.ts
@@ -1,9 +1,9 @@
 import { PagerPathProvider } from './pager-path-provider.model';
 
 export class PagerPathProviders {
-  public default: PagerPathProvider = { getPagerPath: () => undefined };
+  public static default: PagerPathProvider = { getPagerPath: () => undefined };
 
-  public withUserId(userId: string): PagerPathProvider {
+  public static withUserId(userId: string): PagerPathProvider {
     return { getPagerPath: () => (userId ? `user/${userId}` : undefined) };
   }
 }
